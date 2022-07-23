3667124fa9f2af113c77c8530d42dbc3b499e4ae
emerson <emerson.cesario50@gmail.com>
2022-07-23 11:13:24 -0300

    mod files

--- build_rocky.sh
+++ build_rocky.sh
@@ -7,7 +7,7 @@ RUN_OPTS="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
 
 docker run -ti $RUN_OPTS --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro -p 8080:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE $INIT
 
-docker exec $DOCKER_CONTAINER_NAME ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --extra-vars "zabbix_version=5.0 zabbix_server_database_long=pgsql zabbix_server_database=pgsql"
+docker exec $DOCKER_CONTAINER_NAME ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml --extra-vars "zabbix_version=4.4 zabbix_server_database_long=mysql zabbix_server_database=mysql"
 
 docker exec -it $DOCKER_CONTAINER_NAME /bin/bash
 
